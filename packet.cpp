74ad476a1b86212c1b2af2ffe0aa92ac27e47fb7
Rubyboat <rudy@rudysoliz.com>
2025-01-24 18:10:32 -0600

    missing packet updates for last commit

--- packet.cpp
+++ packet.cpp
@@ -3,27 +3,36 @@
 
 
 HunchPacket HunchPacket::decode(const uint8_t* data) {
-		HunchPacket packet;
+	HunchPacket packet;
 
-		// Read version
-		packet.version = *reinterpret_cast<const uint16_t*>(data);
+	// Read version
+	packet.version = *reinterpret_cast<const uint16_t*>(data);
 
-		// Read x, y, u, v
-		packet.x = *reinterpret_cast<const float*>(data + sizeof(uint16_t));
-		packet.y = *reinterpret_cast<const float*>(data + sizeof(uint16_t) + sizeof(float) * 1);
-		packet.u = *reinterpret_cast<const int32_t*>(data + sizeof(uint16_t) + sizeof(float) * 2);
-		packet.v = *reinterpret_cast<const int32_t*>(data + sizeof(uint16_t) + sizeof(float) * 2 + sizeof(int32_t));
+	// Read x, y, u, v
+	packet.x = *reinterpret_cast<const float*>(data + sizeof(uint16_t));
+	packet.y = *reinterpret_cast<const float*>(data + sizeof(uint16_t) + sizeof(float) * 1);
+	packet.u = *reinterpret_cast<const int32_t*>(data + sizeof(uint16_t) + sizeof(float) * 2);
+	packet.v = *reinterpret_cast<const int32_t*>(data + sizeof(uint16_t) + sizeof(float) * 2 + sizeof(int32_t));
 
-		// Read flags
-		packet.flags = *reinterpret_cast<const uint64_t*>(data + sizeof(uint16_t) + sizeof(float) * 2 + sizeof(int32_t) * 2);
+	// Read flags
+	packet.flags = *reinterpret_cast<const uint64_t*>(data + sizeof(uint16_t) + sizeof(float) * 2 + sizeof(int32_t) * 2);
 
-		// Read message
-		const char* messageStart = reinterpret_cast<const char*>(data + sizeof(uint16_t) + sizeof(float) * 2 + sizeof(int32_t) * 2 + sizeof(uint64_t));
-		std::strncpy(packet.message, messageStart, sizeof(packet.message) - 1);
+	// Read message
+	const char* messageStart = reinterpret_cast<const char*>(data + sizeof(uint16_t) + sizeof(float) * 2 + sizeof(int32_t) * 2 + sizeof(uint64_t));
+	std::strncpy(packet.message, messageStart, sizeof(packet.message) - 1);
 
-		// Ensure null termination
-		packet.message[sizeof(packet.message) - 1] = '\0';
+	// Ensure null termination
+	packet.message[sizeof(packet.message) - 1] = '\0';
 
-		return packet;
+	return packet;
 }
 
+HunchPacket *HunchPacket::ofMessage(const char* message)
+{
+  auto packet = new HunchPacket();
+
+  std::strncpy(packet->message, message, 1);
+  packet->flags = ClientFlags::LOG;
+
+  return packet;
+}
--- packet.h
+++ packet.h
@@ -10,6 +10,17 @@ struct HunchPacket {
     char message[1024];
 
     static HunchPacket decode(const uint8_t* data);
+    static HunchPacket* ofMessage(const char* message);
 };
 #pragma pack(pop)
 
+struct ClientFlags {
+    const static uint64_t SENDING_PICTURE = 1;
+    const static uint64_t LOG = 4;
+};
+
+struct ServerFlags {
+    const static uint64_t REQUEST_IMAGE = 1;
+    const static uint64_t DONT_INTERPRET_MOTORS = 2;
+    const static uint64_t LOG = 4;
+};
